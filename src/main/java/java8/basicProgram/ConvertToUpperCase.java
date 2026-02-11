846254defed6d9f22ed5879e1b66d6e541fbf3f0
Anil <anilcse2005@gmail.com>
2026-02-11 13:41:17 +0530

    Add uppercase program

--- src/main/java/java8/basicProgram/ConvertToUpperCase.java
+++ src/main/java/java8/basicProgram/ConvertToUpperCase.java
@@ -1,4 +1,13 @@
 package java8.basicProgram;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ConvertToUpperCase {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("java", "python", "c++", "javascript");
+        list.stream()
+            .map(String::toUpperCase)
+            .forEach(System.out::println);
+    }
 }
